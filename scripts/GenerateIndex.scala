193a42a5441802217ce79b38dd10ad1e3fa1b2e7
Gaurav Vaidya <gaurav@renci.org>
2023-05-25 11:00:01 -0400

    Extract basic info from XML files.

--- scripts/GenerateIndex.scala
+++ scripts/GenerateIndex.scala
@@ -1,7 +1,10 @@
 //> using dep com.typesafe.scala-logging::scala-logging:3.9.5
 //> using dep ch.qos.logback:logback-classic:1.4.7
+//> using dep org.scala-lang.modules::scala-xml:2.1.0
 
 import java.io.File
+import scala.xml.XML
+
 import com.typesafe.scalalogging.StrictLogging
 
 object GenerateIndex extends StrictLogging {
@@ -16,19 +19,36 @@ object GenerateIndex extends StrictLogging {
 
     logger.info(s"Identified ${xmlFiles.length} XML files to index.")
 
-    xmlFiles.foreach(indexFile)
+    xmlFiles.flatMap(indexFile).foreach(res => {
+      println(s"${res.file}\t${res.rootLabel}\t${res.id}\t${res.url}\t${res.description}")
+    })
   }
 
   /** Data types */
   case class Result(
     /** The file being indexed. */
-    file: File
+    file: File,
+    rootLabel: String,
+    id: String,
+    url: String,
+    description: String
   )
 
   /** Index files */
   def indexFile(f: File): Option[Result] = {
     logger.debug(s"Indexing file ${f}")
-    None
+    try {
+      val xml = XML.loadFile(f)
+      val id = (xml \ "id" \ "@value").text
+      val url = (xml \ "url" \ "@value").text
+      val description = (xml \ "description" \ "@value").text
+      Some(Result(f, xml.label, id, url, description))
+    } catch {
+      case ex => {
+        ex.printStackTrace()
+        None
+      }
+    }
   }
 
   /** Recurse from files in a given directory.
